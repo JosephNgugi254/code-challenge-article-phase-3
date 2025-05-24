3dc57d2da4e239823b1a0294b0f1ab728030c4cc
{JosephNgugi254} <{solitaryjoe069@gmail.com}>
2025-05-24 14:14:32 +0300

    edited debug.py

--- lib/debug.py
+++ lib/debug.py
@@ -1,4 +1,11 @@
 # lib/debug.py
+
+import sys
+import os
+
+# Add the project root to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from lib.models.author import Author
 from lib.models.magazine import Magazine
 from lib.models.article import Article
